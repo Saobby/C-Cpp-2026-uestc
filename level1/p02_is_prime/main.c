d0fc55bebad1f80152c414330caf3412576d2bf1
Saobby <101960076+saobby@users.noreply.github.com>
2026-09-16 20:10:37 +0800

    实现质数判断

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("hello world!\n");
+    int my_num;
+    printf("Enter a number: ");
+    scanf("%d", &my_num);
+
+    int is_prime = 1;
+
+    for (int i = 2; i <= sqrt(my_num); i++) {
+        if (my_num % i == 0) {
+            is_prime = 0;
+            break;
+        }
+    }
+
+    if (is_prime) {
+        printf("%d is a prime number.\n", my_num);
+    } else {
+        printf("%d is not a prime number.\n", my_num);
+    }
+
     return 0;
 }
