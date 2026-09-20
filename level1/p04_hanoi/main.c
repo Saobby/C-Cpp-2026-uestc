5946ff3a87f889fe05632b32d526b55e801498fd
Saobby <101960076+saobby@users.noreply.github.com>
2026-09-20 09:02:56 +0800

    实现汉诺塔游戏

--- level1/p04_hanoi/main.c
+++ level1/p04_hanoi/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int move_hanoi(int height, char from, char dest, char temp) {
+    if (height == 1) {
+        printf("%c -> %c\n", from, dest);
+    }else {
+        move_hanoi(height-1, from, temp, dest);
+        move_hanoi(1, from, dest, temp);
+        move_hanoi(height-1, temp, dest, from);
+    }
+    return 0;
+}
+
 int main() {
-    printf("hello world!\n");
+    move_hanoi(4, 'A', 'C', 'B');
     return 0;
 }
