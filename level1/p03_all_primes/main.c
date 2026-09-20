e1747a14c9456c4991dad72af5d267bd1ae28bc1
Saobby <101960076+saobby@users.noreply.github.com>
2026-09-20 08:51:36 +0800

    实现打印2-1000质数

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int is_prime(int my_num) {
+    for (int i = 2; i <= sqrt(my_num); i++) {
+        if (my_num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int main() {
-    printf("hello world!\n");
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    for (int i=2; i<=1000; i++) {
+        if (is_prime(i)) {
+            printf("%d\n", i);
+        }
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("%ld us\n", (end.tv_nsec-start.tv_nsec) / 1000);
     return 0;
 }
