62b35f0f5372931bc12b022f63b2fb5f3c03ee0e
Saobby <101960076+saobby@users.noreply.github.com>
2026-09-23 21:27:44 +0800

    p05实现游戏主循环，玩家移动和胜利判定

--- level1/p05_maze/main.c
+++ level1/p05_maze/main.c
@@ -1,8 +1,16 @@
+// 说明:
+// 该程序在Linux上测试可用
+// 请直接在终端运行该程序构建得到的二进制文件
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <termios.h>
+#include <unistd.h>
 
 static int maze_size = 9;
+
+int player_pos[2] = {1, 1};
 
 int randint(int min, int max) {
     return rand() % (max - min + 1) + min;
@@ -114,22 +122,99 @@ int *gen_maze() {
     return maze;
 }
 
-int main() {
-    srand(time(NULL));
-
-    int *maze = gen_maze();
-
+int print_maze(int *maze) {
+    printf("#:墙 P:玩家 E:出口\n");
+    printf("WASD控制玩家移动\n\n");
     for (int y = 0; y < maze_size * 2 + 1; y++) {
         for (int x = 0; x < maze_size * 2 + 1; x++) {
             int idx = y * (maze_size * 2 + 1) + x;
             if (maze[idx] == 1) {
                 printf("#");
+            } else if (player_pos[0] == x && player_pos[1] == y) {
+                printf("P");
+            } else if (x == maze_size * 2 - 1 && y == maze_size * 2 - 1) {
+                printf("E");
             } else {
                 printf(" ");
             }
         }
         printf("\n");
     }
+    return 0;
+}
 
+char get_input() {
+    char c;
+    read(STDIN_FILENO, &c, 1);
+    return c;
+}
+
+int move(int direction, int *maze, int *pos) {
+    int x = pos[0];
+    int y = pos[1];
+
+    if (direction == 0) {  // 上
+        y--;
+    }else if (direction == 1) {  // 下
+        y++;
+    }else if (direction == 2) {  // 左
+        x--;
+    }else if (direction == 3) {  // 右
+        x++;
+    }
+
+    if (maze[y * (maze_size * 2 + 1) + x] == 0) {
+        pos[0] = x;
+        pos[1] = y;
+        if (x == maze_size * 2 - 1 && y == maze_size * 2 - 1) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    srand(time(NULL));
+
+    struct termios old_term, new_term;
+    tcgetattr(STDIN_FILENO, &old_term);
+    new_term = old_term;
+    new_term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+
+    int *my_maze = gen_maze();
+
+    int result = -1;
+    while (1) {
+        system("clear");
+        print_maze(my_maze);
+        char input = get_input();
+        result = -1;
+        if (input == 'w') {
+            result = move(0, my_maze, player_pos);
+        } else if (input == 's') {
+            result = move(1, my_maze, player_pos);
+        } else if (input == 'a') {
+            result = move(2, my_maze, player_pos);
+        } else if (input == 'd') {
+            result = move(3, my_maze, player_pos);
+        } else if (input == 'q') {
+            result = 2;
+            break;
+        }
+        if (result == 1) {
+            break;
+        }
+    }
+
+    system("clear");
+    if (result == 2) {
+        printf("游戏已退出!\n");
+    }else if (result == 1) {
+        printf("恭喜你，成功走出迷宫!\n");
+    }
+
+    free(my_maze);
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
     return 0;
 }
